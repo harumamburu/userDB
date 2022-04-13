8b1bedb6aca03f83e3aacf993c5bdf93bb4c9040
harumamburu <nikolay.kovalev92@gmail.com>
2022-04-13 22:09:30 +0300

    Remove default data

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import UserForm from "./components/users/userform/UserForm";
 import UserList from "./components/users/userlist/UserList";
 
 function App() {
-  const [users, setUsers] = useState([{id: "u1", name: "Jhon Doe", age: "55"}]);
+  const [users, setUsers] = useState([]);
 
   return (
     <div>
       <UserForm onNewUser={user => setUsers(oldUsers => [...oldUsers, user])} />
-      <UserList users={users} />
+      {users.length && <UserList users={users} />}
     </div>
   );
 }
